819c1b1ecf594e8de0fda363e96d549a4be133ac
Jeffrey Lee <darkthread@gmail.com>
2022-02-03 18:18:28 +0800

    prototype

--- Program.cs
+++ Program.cs
@@ -6,15 +6,45 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.Urls.Add("http://127.0.0.1:0");
 
-app.UseFileServer(new FileServerOptions {
+app.UseFileServer(new FileServerOptions
+{
     RequestPath = "",
     FileProvider = new Microsoft.Extensions.FileProviders
                     .ManifestEmbeddedFileProvider(
         typeof(Program).Assembly, "ui"
-    ) 
+    )
 });
 
 app.MapPost("/newguid", () => Guid.NewGuid().ToString());
+
+int sseCnnCount = 0;
+bool sseTriggered = false;
+int bps = 4;
+
+app.MapGet("/sse", async (context) =>
+{
+    sseTriggered = true;
+    var resp = context.Response;
+    Interlocked.Increment(ref sseCnnCount);
+    resp.Headers.Add("Content-Type", "text/event-stream");
+    try
+    {
+        //set reconnetion timeout
+        await resp.WriteAsync($"retry: {1000 / bps}\n\n");
+        for (int i = 0; i < 60 * bps; i++)
+        {
+            await resp.WriteAsync($"data: {i}\n\n");
+            await resp.Body.FlushAsync();
+            if (context.RequestAborted.IsCancellationRequested)
+                break;
+            await Task.Delay(1000 / bps);
+        }
+    }
+    finally
+    {
+        Interlocked.Decrement(ref sseCnnCount);
+    }
+});
 
 var task = app.RunAsync();
 
@@ -26,5 +56,20 @@ if (!Debugger.IsAttached)
     Process.Start(new ProcessStartInfo("cmd", $"/c start {url}")
     { CreateNoWindow = true });
 }
+
+// watch dog
+var appLife = app.Services.GetRequiredService<IHostApplicationLifetime>();
+Task.Factory.StartNew(async () => {
+    int zeroCount = 0;
+    while (!sseTriggered) {
+        await Task.Delay(1000);
+    }
+    while (zeroCount <= 2 * bps) {
+        if (sseCnnCount != 0) zeroCount = 0;
+        else zeroCount++;
+        await Task.Delay(1000 / bps);
+    }
+    appLife.StopApplication();
+});
 
 task.Wait();
